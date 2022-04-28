e36dd162ffafa3f584429263df0e2f7b3727e071
ELIEL <cg3015467@LAB117M06.IFSP>
2022-04-28 17:47:12 -0300

    Tamo indo

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,11 +1,13 @@
 import "./Header.css";
 import Logo from "../Logo/Logo";
+import Nav from "../NavBar/Nav";
 
 
 function Header() {
     return (
         <div className="header-nav">
             <Logo />
+            <Nav />
         </div>
     );
 }
--- src/Components/Home/NavBar/Nav.js
+++ src/Components/Home/NavBar/Nav.js
@@ -4,19 +4,8 @@ import "./Nav.css"
 function Nav ()  {
     return( 
         <>
-        <li class="nav-item">
-            <a class="nav-link mr-1" style="cursor: pointer;">
-                requerimentos
-            </a>
-        </li>
 
-        <div class="text-center options" id="mySidebar">
-            <a href="{{ route('amostra.index') }}" class="mr-2">amostra</a>
-            <a href="{{ route('funcionario.index') }}" class="mr-2">funcionário</a>
-            <a href="{{ route('layout.index') }}" class="mr-2">layout</a>
-            <a href="{{ route('reposicao.index') }}" class="mr-2">reposição</a>
-            <a href="{{ route('frete.index') }}">frete</a>
-        </div>
+       
         </>
     )
 };
