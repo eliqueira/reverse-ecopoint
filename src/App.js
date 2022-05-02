086ec7ed0b3b12c8a1ec5d320f22ec8bd5880961
Eliel Siqueira <elielkaragua2016@gmail.com>
2022-05-02 17:38:16 -0300

    eliel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
 import Home from './Components/Home/Home';
+import { Routes, Route} from "react-router-dom";
+import Logo from 'Components/Home/Logo/Logo';
+import Nav from 'Components/Home/NavBar/Nav';
+import FormUser from 'Components/Home/FormUser';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+
+
+  return (
+    // <LangContext.Provider value={[
+    //   currentLang === 'br' 
+    //   ? lang.br 
+    //   : lang.en,
+    //   toggleLanguage,
+    //   setCurrentLanguage
+    // ]}>
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="logo" element={<Logo/>} />
+        <Route path="nav" element={<Nav/>} />
+        <Route path="form" element={<FormUser/>} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App
+
+
 //   const lang = {
 //     br: {
 //       welcome: 'Bem-Vindo',
@@ -26,10 +56,6 @@ function App() {
 //   const newLanguage = currentLang === 'br' ? 'en' : 'br'
 //   setCurrentLanguage(newLanguage)
 //  }
-
-  return (
-    <div>
-      <Home />
       {/* <LangContext.Provider value={[
           currentLang === 'br' 
           ? lang.br 
@@ -42,8 +68,3 @@ function App() {
         </div>
       </LangContext.Provider> */}
       {/* <ApiIntegrantion /> */}
-    </div>
-  );
-}
-
-export default App
